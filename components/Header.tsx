558e0145d5ea7718277a72c73b1fabef35c9fc5f
samiulislam09 <samiulislamsawon09@gmail.com>
2022-06-26 21:42:23 +0600

    added card section and displayed data to ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,69 @@
 import Link from "next/link";
 
 function Header() {
-    return (
-        <div>
-            <header className="mx-auto flex justify-between p-10 max-w-7xl">
-                <Link href='/'><img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" /></Link>
-                <div className="flex">
-                    <menu className="hidden md:inline-flex space-x-5 items-center">
-                        <Link href='/our-story'><a>Our story</a></Link>
-                        <Link href='/'><a>Membership</a></Link>
-                        <Link href='/'><a>Write</a></Link>
-                        <Link href='/'><a>Sign In</a></Link>
-                    </menu>
-                    <Link href='/'><a className="rounded-full bg-black text-white py-2 px-4 mx-4">Get Started</a></Link>
-                </div>
-            </header>
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">Blog</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Login</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings';
@@ -9,10 +10,32 @@ export default function Home({ posts }: Props) {
   console.log(posts);
   
   return (
-    <div className="">
+    <div className="bg-white">
       <Header></Header>
+      <div className='grid md:grid-cols-2 items-center'>
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="card lg:card-side bg-base-100 shadow-xl m-4 max-w-2xl">
+              <figure>
+                <img
+                  src="https://api.lorem.space/image/album"
+                  className='md:w-52'
+                  alt="Album"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{post.title}</h2>
+                <p>{post.description}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Read More</button>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 
